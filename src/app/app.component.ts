034ce17266a2c9eee692fe579d52625047e9b7d5
Anzen Digital <alex.c.valera94@hotmail.com>
2019-03-06 17:35:36 -0600

    Inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Ingreso } from './ingreso/ingreso.model';
+import { Egreso } from './egreso/egreso.model';
+import { IngresoService } from './ingreso/ingreso.service';
+import { EgresoService } from './egreso/egreso.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'presupuesto-app';
+  ingresos: Ingreso[] = [];
+  egresos: Egreso[] = [];
+
+  constructor(private ingresoService: IngresoService,
+      private egresoService: EgresoService) {
+    this.ingresos = ingresoService.ingresos;
+    this.egresos = egresoService.egresos;
+  }
+
+  getIngresoTotal() {
+    let ingresoTotal = 0;
+    this.ingresos.forEach(ingreso => {
+      ingresoTotal += ingreso.valor;
+    });
+
+    return ingresoTotal;
+  }
+
+  getEgresoTotal() {
+    let egresoTotal = 0;
+    this.egresos.forEach(egreso => {
+      egresoTotal += egreso.valor;
+    });
+
+    return egresoTotal;
+  }
+
+  getPorcentajeTotal() {
+    return this.getEgresoTotal() / this.getIngresoTotal();
+  }
+
+  getPresupuestoTotal() {
+    return this.getIngresoTotal() - this.getEgresoTotal();
+  }
 }
